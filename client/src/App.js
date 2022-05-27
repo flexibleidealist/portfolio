eb7ce3194238272d01931b964194fbbc2b9618c4
Nicholas Rynearson <nrynearson@gmail.com>
2022-05-27 16:06:18 -0400

    creates Contact component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { About } from './components/About.jsx';
+import { Contact } from './components/Contact.jsx';
 import { Projects } from './components/Projects.jsx';
 function App() {
   return (
     <div className="App">
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+export const Contact = () => {
+  return (
+    <div className="contact">
+      <h3>let's work together!</h3>
+      <a href="https://www.linkedin.com/in/nicholas-rynearson-88a77635/">
+        LinkedIn
+      </a>
+      <a href="https://github.com/flexibleidealist">GitHub</a>
+      <a href="mailto:nrynearson@gmail.com">E-mail</a>
+    </div>
+  );
+};
