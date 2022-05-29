f4878cec9144fe4dd9d033a10c19aadbcd41bc03
Nicholas Rynearson <nrynearson@gmail.com>
2022-05-29 17:30:25 -0400

    continues work on styling; fixes project looping bug

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,13 +4,17 @@ import '../App.css';
 export const About = () => {
   return (
     <div className="about section-container">
-      <img id="profile-img" src={profileImage} alt="Nicholas' head shot" />
-      <div className="about-info">
+      <div className="about-header">
         <h1>Nicholas Rynearson</h1>
         <h2>software engineer | full-stack developer</h2>
+      </div>
+      <img id="profile-img" src={profileImage} alt="Nicholas' head shot" />
+      <div className="about-info">
         <p>{aboutText}</p>
         <p>{currentRole}</p>
-        <a href="../nrynearson-resume.pdf">download my resume</a>
+        <a href="../nrynearson-resume.pdf">
+          <button className="button-43">download my resume</button>
+        </a>
       </div>
     </div>
   );
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import { Project } from './Project.jsx';
 import { projects } from '../data.js';
 import '../App.css';
 
 export const Projects = () => {
+  const [currentProject, setCurrentProject] = useState(projects[0]);
+  const goBack = () => {
+    let currentIndex = projects.indexOf(currentProject);
+    if (currentIndex === 0) setCurrentProject(projects[projects.length - 1]);
+    else setCurrentProject(projects[currentIndex - 1]);
+  };
+  const goForward = () => {
+    let currentIndex = projects.indexOf(currentProject);
+    if (currentIndex === projects.length - 1) {
+      setCurrentProject(projects[0]);
+    } else setCurrentProject(projects[currentIndex + 1]);
+  };
   return (
     <div className="projects section-container">
       <h2>recent projects:</h2>
-      <div className="projects-container">
-        {projects.map(project => (
-          <Project project={project} key={project.name} />
-        ))}
+      <Project project={currentProject} />
+      <div className="button-container">
+        <button className="button-43" onClick={goBack}>
+          Previous
+        </button>
+        <button className="button-43" onClick={goForward}>
+          Next
+        </button>
       </div>
     </div>
   );
